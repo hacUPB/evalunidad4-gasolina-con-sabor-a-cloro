a57bf09fa6165435b4166b54a33935fc859121ca
jamayav <juan.amayav@upb.edu.co>
2022-11-14 17:56:49 -0500

    falta corregir el orden en el que el programa tira las cosas

--- main.c
+++ main.c
@@ -2,43 +2,56 @@
 #include <semaphore.h>
 #include <stdio.h>
 
-int buffer[10];
+int buffer = 0 ;
 int turno;
 char letra;
+char infocuenta[20];
+sem_t empty,full;  
 sem_t sem_prod;
 sem_t sem_con;
+pthread_mutex_t mutex; 
+
 
 void* productor(void* data)
 {
-        if ( buffer[9] ){
-			sem_wait(&sem_prod);
-		}else {
-			sem_post(&sem_prod);
-		}
-		pthread_exit(NULL);
+       while(1){  
+        sem_wait(&empty);  
+        for (size_t i = 0; i < 9; i++)
+	    {
+            pthread_mutex_lock(&mutex);
+			
+            printf( "que ocpion desea elegir A:abrir una cuenta \n ");
+			scanf("%c",&letra);
+            buffer++;
+            printf(" El turno del cliente %d pra la solicitud %c\n ", buffer, letra) ;
+            pthread_mutex_unlock(&mutex);
+			
+	    }  
+        sem_post(&full);        
+        return NULL;
+    }  
 }
-
 void* consumidor(void* data)
 {
-        for(int i = 0 ; i > 10 ; i++){
-			if(buffer[i]  == 0 ){
-				sem_wait(&sem_con);
-				break;
-			}else{
-				sem_post(&sem_con);
-				break;
-			}
-		}
-		printf("A: abrir una cuenta \n");
-		scanf("%c" ,&letra);
-		for (int i= 0 ; i < 10 ; i++){
-			if (buffer [i] == 0){
-				turno = i;
-				printf("Su turno es %d %c", (turno + 1), letra);
-				
-			}break;
-		}
-		pthread_exit(NULL);
+	
+       while(1)
+        {
+        sem_wait(&empty); 
+        for (size_t i = 0; i < 9; i++)
+	    {
+            pthread_mutex_lock(&mutex);
+            printf( " la caja va a atender %d \n ",buffer);  
+            buffer--;
+            printf(" rellene la informacion para abrir la cuenta \n ") ;
+			scanf("%s" ,infocuenta);
+			printf("el nombre de su cuenta sera %s \n ", infocuenta);
+            pthread_mutex_unlock(&mutex);
+			
+	    }    
+        
+        sem_post(&full);        
+        return NULL;
+    	}	
 		
        
 }
@@ -46,15 +59,33 @@ void* consumidor(void* data)
 
 int main(int argc, char** argv)
 {
-        pthread_t prod;
-        pthread_t con;
+    
+	
+		    pthread_t thrd_prod , thrd_cons;  
 
-        sem_init(&sem_prod,0,5);
-        sem_init(&sem_con,0,0);
+    pthread_mutex_init( &mutex , NULL );     
+    
+    sem_init (&empty, 0, 5);            
+    sem_init (&full, 0, 0);        
+    
+    if( pthread_create( &thrd_prod , NULL, productor ,  NULL ) != 0 )  {
+		printf( "thread create failed." );
+	}
+        
+	if( pthread_create( &thrd_cons , NULL, consumidor ,   NULL ) != 0 ) {
+		printf( "thread create failed." );  
+	}
+       
 
-        pthread_create(&prod,NULL,productor,NULL);
-        pthread_create(&con,NULL,consumidor,NULL);
-
-        pthread_join(con,NULL);
-        return 0;
+     
+    if( pthread_join( thrd_prod , NULL ) != 0 )  
+        printf( " wait thread failed.");  
+    if( pthread_join( thrd_cons , NULL ) != 0 )  
+        printf( " wait thread failed.");  
+    sem_destroy (&full);            
+    sem_destroy(&empty);            
+    pthread_mutex_destroy( &mutex );          
+    return 0;  
+	
+	
 }
